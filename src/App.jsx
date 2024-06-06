8671bde87b4f3076ee3d205cd0f4421a4e662f6e
sahikkushwaha51 <sahilkushwaha790@gmail.com>
2024-06-06 12:07:20 +0530

    add

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+
+
+
+import './App.css'
+import CalcClass from './Project/CalcClass'
+import Counter from './Project/Counter'
+import Es5 from './Project/Es5'
+
+function App() {
+  
+
+  return (
+    <>
+     {/* assingment */}
+     <Es5/>
+     <CalcClass/>
+     <Counter/>
+
+
+
+
+
+
+     
+    </>
+  )
+}
+
+export default App

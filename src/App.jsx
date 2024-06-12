45651c20a07e7140683d6b17fc440c37a4553e99
sahikkushwaha51 <sahilkushwaha790@gmail.com>
2024-06-12 11:57:28 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,46 @@
 
 
 
-import './App.css'
-import CalcClass from './Project/CalcClass'
-import Counter from './Project/Counter'
-import Es5 from './Project/Es5'
+// import './App.css'
+import Content from './Project/Component/Content'
+import Footer from './Project/Component/Footer'
+import Header from './Project/Component/Header'
+
+// Component
+
+
+
+// import CalcClass from './Project/CalcClass'
+// import Counter from './Project/Counter'
+// import Es5 from './Project/Es5'
+// import OnChangeTextFun from './Project/OnChangeTextClass'
+// import Onchange from './Project/Onchange'
+// import Event from './Project/Event/Event'
 
 function App() {
-  
+
 
   return (
     <>
-     {/* assingment */}
-     <Es5/>
-     <CalcClass/>
-     <Counter/>
+      {/* assingment */}
+      {/* <Es5/> */}
+      {/* <CalcClass/> */}
+      {/* <Counter/> */}
+      {/* <OnChangeTextFun/> */}
+      {/* <Onchange/> */}
+      {/* <Event/> */}
+
+
+
+      {/* Counter */}
+      <Header />
+      <Footer/>
+      <Content/>
 
 
 
 
 
-
-     
     </>
   )
 }
---
+++ src/Project/Component/Header.jsx
@@ -0,0 +1,28 @@
+
+import './Style.css'
+
+
+
+export default function Header() {
+
+    return (
+        <>
+            <nav>
+                <div className="header">
+                    <div className="logo">
+
+                        <h1>JW Solutions</h1>
+
+                        
+
+
+
+
+
+
+                    </div>
+                </div>
+            </nav>
+        </>
+    )
+}
